36d5dc8a37412c5fac1f67cede7f97538eb64d8b
bharatchodvadiya <chodvadiya.bharat@yahoo.com>
2012-08-03 19:17:23 +0530

    Image resizing when upload
    Image resizing when upload

--- bpc_slideshow/upload.php
+++ bpc_slideshow/upload.php
@@ -1,6 +1,14 @@
 <?php
  include('../../../wp-load.php');
 
+ function getExtension($str) {
+         $i = strrpos($str,".");
+         if (!$i) { return ""; }
+         $l = strlen($str) - $i;
+         $ext = substr($str,$i+1,$l);
+         return $ext;
+ }
+ 
 if (!empty($_FILES)) 
 {
     if (file_exists("images/" . $_FILES["file"]["name"]))
@@ -9,8 +17,54 @@ if (!empty($_FILES))
     }
     else
     {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "images/" . $_FILES["file"]["name"]);     
+		$name = $_FILES["file"]["name"];
+		$uploadedfile = $_FILES['file']['tmp_name'];
+		$ext = getExtension($name);
+ 		$ext = strtolower($ext);
+					
+		if($ext=="jpg" || $ext=="jpeg" )
+							{
+								$src = imagecreatefromjpeg($uploadedfile);
+							}
+							else if($ext=="png")
+							{
+								$src = imagecreatefrompng($uploadedfile);
+							}
+							else 
+							{
+								$src = imagecreatefromgif($uploadedfile);
+							}
+							
+		
+		list($width,$height)=getimagesize($uploadedfile);
+					
+		if(get_option('slideshow_image_width') != '')
+		{
+		$newwidth=get_option('slideshow_image_width');
+		}
+		else
+		{
+		$newwidth=240;
+		}
+		
+		if(get_option('slideshow_image_height') != '')
+		{
+		$newheight=get_option('slideshow_image_height');
+		}
+		else
+		{
+		$newheight=160;
+		}
+		
+		
+		$tmp=imagecreatetruecolor($newwidth,$newheight);
+		imagecopyresampled($tmp,$src,0,0,0,0,$newwidth,$newheight,$width,$height);
+		
+		$filename = "images/".$_FILES["file"]["name"];
+		
+		if(imagejpeg($tmp,$filename))
+			echo "done";
+									     
     }
 }
 
